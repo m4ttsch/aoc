46418ba1ce41de268c22baca109ecd73c1aaa77f
Matthew Schlenker <m4ttschlenker@gmail.com>
2023-12-03 22:12:31 -0800

    Day 4.

--- src/4/1.ts
+++ src/4/1.ts
@@ -1,1 +1,11 @@
-export default function solve() {}
+export default function solve(input: string) {
+  const cards = input.trim().split("\n");
+
+  return cards.reduce((sum, line) => {
+    const nums = [...line.split(": ")[1].matchAll(/\d+/g)].map((m) => m[0]);
+    const uniq = new Set(nums).size;
+    const numMatches = nums.length - uniq;
+
+    return numMatches ? sum + 2 ** (numMatches - 1) : sum;
+  }, 0);
+}
--- src/4/2.ts
+++ src/4/2.ts
@@ -1,1 +1,18 @@
-export default function solve() {}
+export default function solve(input: string) {
+  const cards = input.trim().split("\n");
+  const counts = new Array(cards.length).fill(1);
+
+  for (let i = 0; i < counts.length; i++) {
+    const nums = [...cards[i].split(": ")[1].matchAll(/\d+/g)].map((m) => m[0]);
+    const uniq = new Set(nums).size;
+    const numMatches = nums.length - uniq;
+
+    for (let k = 1; k <= numMatches; k++) {
+      if (i + k < counts.length) {
+        counts[i + k] += counts[i];
+      }
+    }
+  }
+
+  return counts.reduce((sum, el) => sum + el);
+}
